5abbef824e036224f1b71a8ea244553b38a54f13
Timothy Mwangi <timomwa@gmail.com>
2016-10-11 20:17:18 +0300

    Quit cacheing pages

--- src/main/java/co/ke/technovation/pages/PageBean.java
+++ src/main/java/co/ke/technovation/pages/PageBean.java
@@ -30,7 +30,7 @@ public class PageBean implements Serializable {
 	public String getPage() {
 		final String temp_page = page;
 		page = "home";
-		return page;
+		return temp_page;
 	}
 
 	public void setPage(String page) {
